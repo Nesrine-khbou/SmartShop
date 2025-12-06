9b93033914bb22ddf0da2f37473448455bc497bb
unknown <nesrinekhbou7@gmail.com>
2025-12-06 10:57:46 +0100

    added google-services file and necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +57,15 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // --- Firebase BoM ---
+    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+
+    // --- Firestore ---
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // (OPTIONNEL) Auth si tu en as besoin
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    // (OPTIONNEL) Analytics si voulu
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
